c21983f742e9017a6d1bc750baff4909f45e1d30
Michal Parusinski <mparusinski@googlemail.com>
2010-10-18 13:19:59 +0100

    Updated version of signature.hs

--- signature.hs
+++ signature.hs
@@ -5,6 +5,7 @@ module Signature where
 -- type t denotes the signature i.e. all relations allowed
 -- How do we allow for empty signature i.e. with no relations
 data Modal = Exists | ForAll
+-- e.g. t could be data t = HasChild Int Int | IsParent Int Int
 data Formula t = F | T | Atom Int |
                  Neg (Formula t) | And (Formula t) (Formula t) |
                  Or (Formula t) (Formula t) | Modal t (Formula t)
