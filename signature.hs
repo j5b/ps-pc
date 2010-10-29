23f311aac06a54c8da93ff6f4c311e9701cd76f3
Michal Parusinski <mparusinski@googlemail.com>
2010-10-29 20:31:39 +0100

    Updated proof and model

--- signature.hs
+++ signature.hs
@@ -6,17 +6,32 @@ module Signature where
 -- How do we allow for empty signature i.e. with no relations
 -- data Modal = Exists | ForAll
 -- e.g. t could be data t = HasChild Int Int | IsParent Int Int
-data Formula t = T | Atom Int | Neg (Formula t) | 
-     	       	 And (Formula t) (Formula t) | Exists t (Formula t)
+data Concept t = T | Atom Int | Neg (Concept t) | 
+     	       	 And (Concept t) (Concept t) | Exists t (Concept t)
      deriving (Eq, Show)
 
 top = T
 bottom = Neg T
 
-(&) :: (Formula t) -> (Formula t) -> (Formula t)
+(&) :: (Concept t) -> (Concept t) -> (Concept t)
 f1 & f2 = And f1 f2
 
-(!):: (Formula t) -> (Formula t) -> (Formula t)
+-- this one is NOT IDEAL TODO: Find a symbol
+(!) :: (Concept t) -> (Concept t) -> (Concept t)
 f1 ! f2 = Neg ((Neg f1) & (Neg f2))
 
--- need a way to define Forall
+not :: (Concept t) -> (Concept t)
+not concept = Neg concept
+
+-- find something better than Eq t
+exists :: Eq t => t -> (Concept t) -> (Concept t)
+exists rel concept = Exists rel concept
+
+forall :: Eq t => t -> (Concept t) -> (Concept t)
+forall rel concept = Neg (Exists rel $ Neg concept)
+
+(.>) :: (Concept t) -> (Concept t) -> (Concept t)
+f1 .> f2 = (Neg f1) ! f2
+
+(=:=) :: (Concept t) -> (Concept t) -> (Concept t)
+f1 =:= f2 = (f2 .> f1) & (f1 .> f2)
