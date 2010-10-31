ad37930f60cfc1b3f9b2e7c260f5c735253eb5a6
Michal Parusinski <mparusinski@googlemail.com>
2010-10-31 13:05:40 +0000

    Added license information, a template file for haskell, update model and proof file

--- signature.hs
+++ signature.hs
@@ -1,17 +1,19 @@
--- file: signature.hs
+{- 
+   Author: Michal Gerard Parusinski
+   Maintainer: Michal Gerard Parusinski
+   Email: <mparusinski@googlemail.com>
+   License: GPL 3.0
+   File: signature.hs
+   Description: provides a framework to build and handle description logic concepts
+-}
 
 module Signature where
 
--- import HUnit
-
--- type rel denotes in the signature i.e. all relations allowed
--- How do we allow for empty signature i.e. with no relations?
--- e.g. rels could be data rels = HasChild Int Int | IsParent Int Int
--- Should we create a Relation class?
--- Atom Integer was simply WRONG couldn't express simple C
+-- Concept is the language definition for description logic ALC and AL
 data Concept atoms rels = T | Atom atoms | Neg (Concept atoms rels) | 
      	       	          And (Concept atoms rels) (Concept atoms rels) | Exists rels (Concept atoms rels)
      deriving (Show, Eq)
+
 {- 
    WARNING: Eq is dangerous since it only checks if two statements 
    are identical in the restricted language but not equivalence
@@ -22,35 +24,44 @@ data Concept atoms rels = T | Atom atoms | Neg (Concept atoms rels) |
    but sometimes they are in the restricted language    
 -}
 
+-- Truth/Tautology
 top = T
+-- Bottom/Falsity
 bottom = Neg T
 
--- Still doesn't satify binding rule
+-- neg negates a concept
 neg :: (Concept a r) -> (Concept a r)
 neg concept = Neg concept
 
+-- atom builds and atomic concept
 atom :: a -> (Concept a r)
 atom atomic = Atom atomic 
 
 infixr 8 /\
+-- /\ is and/intersection in description logic
 (/\) :: (Concept a r) -> (Concept a r) -> (Concept a r)
 f1 /\ f2 = And f1 f2
 
 infixr 7 \/
+-- \/ is or/union in description logic
 (\/) :: (Concept a r) -> (Concept a r) -> (Concept a r)
 f1 \/ f2 = Neg ((Neg f1) /\ (Neg f2))
 
 infixr 6 .>
+-- .> is implies/subset
 (.>) :: (Concept a r) -> (Concept a r) -> (Concept a r)
 f1 .> f2 = (Neg f1) \/ f2
 
 infixr 5 <->
+-- <-> is equivalent/same set
 (<->) :: (Concept a r) -> (Concept a r) -> (Concept a r)
 f1 <-> f2 = (f2 .> f1) /\ (f1 .> f2)
 
+-- exists constructs an exists expression
 exists :: r -> (Concept a r) -> (Concept a r)
 exists rel concept = Exists rel concept
 
+-- forall constructs an forall expression
 forall :: r -> (Concept a r) -> (Concept a r)
 forall rel concept = Neg (Exists rel $ Neg concept)
 
---
+++ template.hs
@@ -0,0 +1,20 @@
+{- 
+   Author: <authors name>
+   Maintainer: <maintainer's name>
+   Email: <maintainer's email>
+   License: GPL
+   File: filename
+   Description: short description
+-}
+
+{-
+   General comments
+-}
+
+-- Function documentation/description (not comments)
+someFunction :: a -> b -> ...
+someFunction = definition
+
+{-
+   Always explain functions just above them so Hackage can help us generate documentation
+-}
