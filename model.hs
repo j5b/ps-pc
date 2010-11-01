55b55684f5a1e1f81a0caacee71e049696ae5ff1
Michal Parusinski <mparusinski@googlemail.com>
2010-11-01 14:41:24 +0000

    Updates for signature

--- model.hs
+++ model.hs
@@ -25,20 +25,23 @@ inModel = elem
 -- the following function checks if name(a,b) holds in the given model
 isInModelBin :: String -> (Individual,Individual) -> Model -> Bool
 isInModelBin name (a,b) (dom, _, binaryModel)
-  | not $ (inModel a dom) && (inModel b dom) = error "Individuals not in domain"
+  | not $ (inModel a dom) && (inModel b dom) = error errorMessage
   | otherwise                                = elem (a,b) $ tryLookup name binaryModel
+  where errorMessage = "Individuals "++(show a)++" and "++(show b)++" not in domain"
 
 -- the following function checks if name(a) holds in the given model
 isInModelUn :: String -> Individual -> Model -> Bool
 isInModelUn name a (dom, unaryModel, _)
-  | not $ inModel a dom = error "Individual not in domain"
+  | not $ inModel a dom = error errorMessage
   | otherwise           = elem a $ tryLookup name unaryModel
+  where errorMessage = "Individual "++ (show a) ++ " not in domain"
 
 -- This function search for a in a list [(a,b)] and returns b if possible
 -- otherwise it displays a meaningful message
-tryLookup a domain
+tryLookup :: String -> [(String,a)] -> a
+tryLookup name domain
   | isJust $ result = fromJust result
-  | otherwise       = error "Couldn't find element when looking-up"
-  where result = lookup a domain
+  | otherwise       = error ("Couldn't find relation/atom"++name++" inside the domain")
+  where result = lookup name domain
 
 
