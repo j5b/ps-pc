4f7603a5df84eecf87716169f5bd2613e12040ba
Michal Parusinski <mparusinski@googlemail.com>
2010-11-01 16:28:55 +0000

    Added some simple testing cases for models

--- model.hs
+++ model.hs
@@ -22,16 +22,20 @@ type Model = (Domain, [UnaryRelation], [BinaryRelation])
 -- Creates an empty model (good for testing)
 emptyModel = ([], [], [])
 
+-- report if the domain is empty
+emptyModelReport = ("Model is fine since we got an empty result", True)
+emr = emptyModelReport
+
 -- checks the domain for errors
-checkDomain :: Model -> Report
-checkDomain (dom, uns, bins) = 
+checkModel :: Model -> Report
+checkModel (dom, uns, bins) = 
   combine reportUns reportBins
-  where reportUns  = title "Unary check" $ checkUnary dom uns
-        reportBins = title "Binary check" $ checkBinary dom bins
+  where reportUns  = title "=> Unary check" $ checkUnary dom uns
+        reportBins = title "=> Binary check" $ checkBinary dom bins
   
 -- checks the domain for errors relating to atoms
 checkUnary :: Domain -> [UnaryRelation] -> Report
-checkUnary dom list = foldl1 combine $ map checker list
+checkUnary dom list = foldl1 combine $ makeCorrect (map checker list) [] [emr] -- [emr] since a list is expceted
   where checker (name, set) = flip buildReport result $ 
                                    "Unary " ++ name ++ 
                                     (if result then " is only containing elements of the domain" 
@@ -40,7 +44,7 @@ checkUnary dom list = foldl1 combine $ map checker list
 
 -- checks the domain for errors relating to binary relations
 checkBinary :: Domain -> [BinaryRelation] -> Report
-checkBinary dom list = foldl1 combine $ map checker list
+checkBinary dom list = foldl1 combine $ makeCorrect (map checker list) [] [emr] -- [emr] since a list is expected
   where checker (name, set) = flip buildReport result $ 
                                    "Binary " ++ name ++ 
                                     (if result then " is only containing elements of the domain" 
@@ -48,6 +52,9 @@ checkBinary dom list = foldl1 combine $ map checker list
           where result      = allElements (map fst set) dom && allElements (map snd set) dom
 
 allElements subList list = and $ map (flip elem list) subList
+
+makeCorrect result exception alternative =
+  if result == exception then alternative else result
 
 
 
