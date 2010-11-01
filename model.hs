dcbf3168b016e7e144cb7be078cafb8433f1feda
Michal Parusinski <mparusinski@googlemail.com>
2010-11-01 16:02:54 +0000

    Modified model consistency checking, still not tested

--- model.hs
+++ model.hs
@@ -10,6 +10,7 @@
 module Model where
 
 import Data.Maybe
+import Report
 
 -- A model is a set of individual for which atomic or binary relations may or may not hold
 type Individual = Integer
@@ -18,30 +19,35 @@ type BinaryRelation = (String, [(Individual,Individual)])
 type UnaryRelation = (String, [Individual])
 type Model = (Domain, [UnaryRelation], [BinaryRelation])
 
--- the following functions check if the individual is part of the domain
-inModel :: Individual -> Domain -> Bool
-inModel = elem
+-- Creates an empty model (good for testing)
+emptyModel = ([], [], [])
 
--- the following function checks if name(a,b) holds in the given model
-isInModelBin :: String -> (Individual,Individual) -> Model -> Bool
-isInModelBin name (a,b) (dom, _, binaryModel)
-  | not $ (inModel a dom) && (inModel b dom) = error errorMessage
-  | otherwise                                = elem (a,b) $ tryLookup name binaryModel
-  where errorMessage = "Individuals "++(show a)++" and "++(show b)++" not in domain"
+-- checks the domain for errors
+checkDomain :: Model -> Report
+checkDomain (dom, uns, bins) = 
+  combine reportUns reportBins
+  where reportUns  = title "Unary check" $ checkUnary dom uns
+        reportBins = title "Binary check" $ checkBinary dom bins
+  
+-- checks the domain for errors relating to atoms
+checkUnary :: Domain -> [UnaryRelation] -> Report
+checkUnary dom list = foldl1 combine $ map checker list
+  where checker (name, set) = flip buildReport result $ 
+                                   "Unary " ++ name ++ 
+                                    (if result then " is only containing elements of the domain" 
+                                               else " is not only containing elements of the domain")  
+          where result      = allElements set dom
 
--- the following function checks if name(a) holds in the given model
-isInModelUn :: String -> Individual -> Model -> Bool
-isInModelUn name a (dom, unaryModel, _)
-  | not $ inModel a dom = error errorMessage
-  | otherwise           = elem a $ tryLookup name unaryModel
-  where errorMessage = "Individual "++ (show a) ++ " not in domain"
+-- checks the domain for errors relating to binary relations
+checkBinary :: Domain -> [BinaryRelation] -> Report
+checkBinary dom list = foldl1 combine $ map checker list
+  where checker (name, set) = flip buildReport result $ 
+                                   "Binary " ++ name ++ 
+                                    (if result then " is only containing elements of the domain" 
+                                               else " is not only containing elements of the domain")  
+          where result      = allElements (map fst set) dom && allElements (map snd set) dom
 
--- This function search for a in a list [(a,b)] and returns b if possible
--- otherwise it displays a meaningful message
-tryLookup :: String -> [(String,a)] -> a
-tryLookup name domain
-  | isJust $ result = fromJust result
-  | otherwise       = error ("Couldn't find relation/atom"++name++" inside the domain")
-  where result = lookup name domain
+allElements subList list = and $ map (flip elem list) subList
+
 
 
