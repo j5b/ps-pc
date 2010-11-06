a36fafc10d66d417f2f8fd1e57397f2b06afd9bf
Jannis Bulian <jannis.bulian@gmail.com>
2010-11-06 01:45:54 +0000

    fixed a couple of things

--- proofsearch.hs
+++ proofsearch.hs
@@ -10,9 +10,9 @@
 -}
 
 import Data.Either
-import model
-import proof
-import signature 
+import Model
+import Proof
+import Signature 
 
 -- despatching function
 --findPOM :: [Concept] -> [Concept] -> Either Model Proof
@@ -25,25 +25,25 @@ conceptSort = id
 -- pre: Assume sorted for now.
 findProofOrModel :: [Concept] -> [Concept] ->  Either Model ProofTree
 findProofOrModel [] _                     = Left ([],[],[])
-findProofOrModel [(T):cs] gamma        = findProofOrModel cs gamma model proof
-findProofOrModel ((Atom c):(Neg (Atom d)):cs) gamma   = if  c == d then Right (NodeOne ((Atom c):(Neg (Atom d)):cs), "bottom", (Atom c))
-                                                        else findProofOrModel cs gamma model proof
-findProofOrModel ((And c d):cs) gamma  = either Left g (findProofOrModel (conceptSort (c:d:cs)) gamma model proof)
+findProofOrModel ((T):cs) gamma        = findProofOrModel cs gamma
+findProofOrModel ((Atom c):(Neg (Atom d)):cs) gamma = if  c == d then Right (NodeOne (((Atom c):(Neg (Atom d)):cs), "bottom", (Atom c)) (NodeZero (Neg T)))
+                                                        else findProofOrModel cs gamma
+findProofOrModel ((And c d):cs) gamma  = either Left (Right . g) (findProofOrModel (conceptSort (c:d:cs)) gamma)
     where
       g pf = NodeOne ((And c d):cs, "and", (And c d)) pf
-findProofOrModel ((Or c d):cs) gamma   = either Left g (findProofOrModel (conceptSort (c:cs)) gamma model proof)
+findProofOrModel ((Or c d):cs) gamma   = either Left g (findProofOrModel (conceptSort (c:cs)) gamma)
     where
-      g pf       = either Left (g' pf) (findProofOrModel (conceptSort (d:cs)) gamma model pf)
+      g pf       = either Left (Right . (g' pf)) (findProofOrModel (conceptSort (d:cs)) gamma)
       g' pf1 pf2 = NodeTwo ((Or c d):cs, "or", (Or c d)) pf1 pf2
-findProofOrModel ((Exists rel c):cs) gamma  = g (map (findProofOrModel with the right aprameters) (filteroutexists ((Exists rel c):cs)))
-    where
-      g rs = if null (rights rs) then join (lefts rs) else head rights
-findProofModel cs gamma  = construct a model
+--findProofOrModel ((Exists rel c):cs) gamma  = g (map (findProofOrModel with the right aprameters) (filteroutexists ((Exists rel c):cs)))
+--    where
+--      g rs = if null (rights rs) then join (lefts rs) else head rights
+--findProofModel cs gamma  = construct a model
 
 
 -- Function documentation/description (not comments)
-someFunction :: a -> b -> ...
-someFunction = definition
+--someFunction :: a -> b -> ...
+--someFunction = definition
 
 {-
    Always explain functions just above them so Hackage can help us generate documentation
