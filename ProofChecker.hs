eb4805f9d0c63e1c40eb6581d8348630b53214b9
Ka Wai Cheng <Ka Wai Cheng@localhost>
2010-11-08 23:00:26 +0000

    Complete apart from error/exception testing

--- ProofChecker.hs
+++ ProofChecker.hs
@@ -11,59 +11,62 @@ module ProofChecker where
 
 import Proof
 import Signature
-import List
+import Data.List
 
--- Public function called by other modules to check a proof tree
-proofChecker :: ProofTree -> [Concept] -> (String, Bool)
-proofChecker prooftree gamma
+{-
+  Called by other modules to check a proof tree
+  Checks initial set of concepts and gamma before checking the proof
+-}
+checkProof :: ProofTree -> [Concept] -> (String, Bool)
+checkProof prooftree gamma
   | length (nub cs) /= length cs =
-      error ("Initial concepts must not contain duplicate concepts")
+      error "Initial concepts must not contain duplicate concepts"
   | length (nub gamma) /= glength =
-      error ("Gamma must not contain duplicate concepts")
-  | not (and (map isNNF cs)) =
-      error ("Concepts are not in negation normal form")
-  | not (and (map isNNF gamma)) =
-      error ("Concepts in gamma are not in negation normal form")
-  | length (intersect gamma cs) /= glength =
-      error ("Concepts in gamma are not in the initial set of concepts")
+      error "Gamma must not contain duplicate concepts"
+  | not $ all isNNF cs =
+      error "Concepts are not in negation normal form"
+  | not $ all isNNF gamma =
+      error "Concepts in gamma are not in negation normal form"
+  | length (gamma `intersect` cs) /= glength =
+      error "Concepts in gamma are not in the initial set of concepts"
   | otherwise =
-      checkProof prooftree gamma
+      checkTree prooftree gamma
   where cs = getConcepts prooftree
         glength = length gamma
 
 {-
   Checks a proof tree shows unsatisfiability and returns true if so
   Else returns messages of incorrect steps at point in proof tree & false
-  Counts step?
+  Pre: Gamma and initial set of concepts do not contain duplicates
 -}
-checkProof :: ProofTree -> [Concept] -> (String, Bool)
-checkProof (NodeZero (Neg T)) _ = ("", True)
-checkProof (NodeZero _) _ =
+checkTree :: ProofTree -> [Concept] -> (String, Bool)
+checkTree (NodeZero (Neg T)) _ = ("", True)
+checkTree (NodeZero _) _ =
   ("This proof tree does not show unsatisfiability", False)
-checkProof (NodeOne step tree) gamma
+checkTree (NodeOne step tree) gamma
   | not success =
       (msg, False)
   | cs /= [] =
       ("There should be 1 resulting set of concepts for NodeOne", False)
   | conceptEquals c (getConcepts tree) =
-      checkProof tree gamma
+      checkTree tree gamma
   | otherwise =
       ("Next step's concepts (" ++ showConceptList (getConcepts tree) ++
        ") do not match the result of applying " ++ rule ++ " rule to (" ++
        showConceptList initialconcepts ++ ")", False)
   where (msg, success, c:cs) = checkProofStep step gamma
         (initialconcepts, rule,_) = step
-checkProof (NodeTwo step t1 t2) gamma
+checkTree (NodeTwo step t1 t2) gamma
   | not success =
       (msg, False)
   | length cs /= 2 =
       ("There should be 2 resulting sets of concepts for applying Or rule to ("
        ++ showConceptList initialconcepts ++ ")",
        False)
-  | conceptEquals (cs !! 0) (getConcepts t1) &&
+  | conceptEquals (head cs) (getConcepts t1) &&
     conceptEquals (cs !! 1) (getConcepts t2) ||
     conceptEquals (cs !! 1) (getConcepts t1) &&
-    conceptEquals (cs !! 0) (getConcepts t2)    =
+    conceptEquals (head cs) (getConcepts t2)    =
       (msg1 ++ msg2, tsuccess1 && tsuccess2)
   | otherwise =
       ("Next step's set of concepts (" ++ showConceptList (getConcepts t1)
@@ -71,41 +74,41 @@ checkProof (NodeTwo step t1 t2) gamma
        ") do not match the results of applying the Or rule to (" ++
        showConceptList initialconcepts ++ ")", False)
   where (msg, success, cs) = checkProofStep step gamma
-        (msg1, tsuccess1) = checkProof t1 gamma
-        (msg2, tsuccess2) = checkProof t2 gamma
+        (msg1, tsuccess1) = checkTree t1 gamma
+        (msg2, tsuccess2) = checkTree t2 gamma
         (initialconcepts, rule, _) = step
 
 {-
   Checks a single proof step, returns true and resulting concepts if correct
   Else returns error message, false and given list of concepts of the step
+  Pre:  set of concept in proofstep and gamma contain no duplicates
+  Post: lists of concepts returned contain no duplicates
 -}
 checkProofStep :: ProofStep -> [Concept] -> (String, Bool, [[Concept]])
 checkProofStep (cs, "Bottom", Atom a) _
-  | elem (Atom a) cs && elem (Neg (Atom a)) cs =
+  | Atom a `elem` cs && Neg (Atom a) `elem` cs =
       ("", True, [[Neg T]])
   | otherwise =
       (showConcept (Atom a) ++ " and " ++ showConcept (Neg (Atom a)) ++
-       " do not both exist in the set of concepts (" ++ showConceptList cs ++
-       ")", False, [cs])
-checkProofStep (cs, "And", And l r) _
-  | elem (And l r) cs =
-      ("", True, [nub ((l:r:cs) \\ [(And l r)])])
-  | otherwise =
-      (showConcept (And l r) ++ " does not exist in the set of concepts (" ++
-       showConceptList cs ++ ")", False, [cs])
-checkProofStep (cs, "Or", Or l r) _
-  | elem (Or l r) cs =
-      ("", True, [nub (l:result), nub (r:result)])
-  | otherwise =
-      (showConcept (Or l r) ++ " does not exist in the set of concepts (" ++
-       showConceptList cs ++ ")", False, [cs])
+       " do not both exist in the set of concepts {" ++ showConceptList cs ++
+       "}", False, [cs])
+checkProofStep (cs, rule, c) gamma
+  | c `elem` cs =
+      checkRule (cs, rule, c) gamma
+  | otherwise   =
+      (showConcept c ++ " does not exist in the set of concepts {" ++
+       showConceptList cs ++ "}", False, [cs])
+
+{-
+  Checks the proof rule can be applied, returns true and resulting concepts if correct
+  Else returns error message, false and given list of concepts of the step
+  Pre: ProofStep concept exists
+-}
+checkRule :: ProofStep -> [Concept] -> (String, Bool, [[Concept]])
+checkRule (cs, "And", And l r) _ = ("", True, [nub $ delete (And l r) (l:r:cs)])
+checkRule (cs, "Or", Or l r) _ = ("", True, [nub (l:result), nub (r:result)])
   where result = delete (Or l r) cs
-checkProofStep (cs, "Exists", (Exists r c)) gamma
-  | elem (Exists r c) cs =
-      ("", True, [nub (c:(fsuccs ++ gamma))])
-  | otherwise =
-      (showConcept (Exists r c) ++ " does not exist in the set of concepts (" ++
-       showConceptList cs ++ ")", False, [cs])
+checkRule (cs, "Exists", Exists r c) gamma = ("", True, [nub $ c:fsuccs ++ gamma])
   where fsuccs = [successor | Forall relation successor <- cs, relation == r]
-checkProofStep (cs, rule, c) _ =
+checkRule (cs, rule, c) _ =
   (rule ++ " rule cannot be applied to " ++ showConcept c ++ "", False, [cs])
--- ProofChecker_test.hs
+++ ProofChecker_test.hs
@@ -11,6 +11,7 @@ import Signature
 import Proof
 import ProofChecker
 import Test.HUnit
+--import Control.OldException
 
 bRule = "Bottom"
 aRule = "And"
@@ -26,7 +27,8 @@ fConcept = Forall "R" (Atom "F")
 
 manyConcepts, gamma :: [Concept]
 manyConcepts = [bConcept, aConcept, oConcept, eConcept, fConcept]
-gamma = [Atom "U", Neg (Atom "U"), And (Atom "V") (Atom "W"), Or (Atom "X") (Atom "Y"), Forall "R" (Atom "Z")]
+gamma = [Atom "U", Neg (Atom "U"), And (Atom "V") (Atom "W"),
+         Or (Atom "X") (Atom "Y"), Forall "R" (Atom "Z")]
 
 -- With gamma
 tree :: ProofTree
@@ -47,17 +49,21 @@ tree = NodeOne (manyConcepts ++ gamma , eRule, eConcept)
                                             bRule, Atom "U")
                                           (NodeZero (Neg T)))))
 
+-- tests
+
 -- Tests for checkProofStep
-btest1 = TestCase (assertEqual "Corret bottom rule proof step"
+btest1 = TestCase (assertEqual "Correct bottom rule proof step"
                    ("", True, [[Neg T]])
-                   (checkProofStep ([Atom "A", bConcept], bRule, Atom "A") []))
-btest2 = TestCase (assertEqual "Incorrect bottom rule proof step"
-                   ("", False, [[Neg (Atom "B"), Atom "A"]])
-                   (checkProofStep ([Neg (Atom "B"), Atom "A"], bRule, Atom "A")
-                    gamma))
-btest3 = TestCase (assertEqual "Corret bottom rule proof step with gamma"
+                   $ checkProofStep ([Atom "A", bConcept], bRule, Atom "A") [])
+btest2 = TestCase (assertEqual "InCorrect bottom rule proof step"
+                   ("Atom A and Not (Atom A) do not both exist in the set of"
+                    ++ " concepts {Not (Atom B), Atom A}", False,
+                    [[Neg (Atom "B"), Atom "A"]])
+                   $ checkProofStep ([Neg (Atom "B"), Atom "A"], bRule, Atom "A")
+                    gamma)
+btest3 = TestCase (assertEqual "Correct bottom rule proof step with gamma"
                    ("", True, [[Neg T]])
-                   (checkProofStep ([Atom "A", bConcept], bRule, Atom "A") gamma))
+                   $ checkProofStep ([Atom "A", bConcept], bRule, Atom "A") gamma)
 
 bottomStepTests = TestList [TestLabel "bottomtest1" btest1,
                             TestLabel "bottomtest2" btest2,
@@ -65,45 +71,48 @@ bottomStepTests = TestList [TestLabel "bottomtest1" btest1,
 
 atest1 = TestCase (assertEqual "Correct and rule proof step"
                    ("", True, [[Atom "A", Atom "B"]])
-                   (checkProofStep ([aConcept], aRule, aConcept) []))
-atest2 = TestCase (assertEqual "Incorrect and rule proof step"
-                   ("", False, [[aConcept]])
-                   (checkProofStep ([aConcept], aRule, And (Atom "A") (Atom "C"))
-                    gamma))
+                   $ checkProofStep ([aConcept], aRule, aConcept) [])
+atest2 = TestCase (assertEqual "InCorrect and rule proof step"
+                   ("(Atom A and Atom C) does not exist in the set of " ++
+                    "concepts {(Atom A and Atom B)}", False, [[aConcept]])
+                   $ checkProofStep ([aConcept], aRule, And (Atom "A") (Atom "C"))
+                    gamma)
 atest3 = TestCase (assertEqual "Correct and rule proof step with gamma"
                    ("", True, [[Atom "A", Atom "B"]])
-                   (checkProofStep ([aConcept], aRule, aConcept) gamma))
+                   $ checkProofStep ([aConcept], aRule, aConcept) gamma)
 
 andStepTests = TestList [TestLabel "andtest1" atest1,
                          TestLabel "andtest2" atest2,
                          TestLabel "andtest3" atest3]
 
-otest1 = TestCase (assertEqual "Corret or rule proof step"
+otest1 = TestCase (assertEqual "Correct or rule proof step"
                    ("", True, [[Atom "C", bConcept], [Atom "D", bConcept]])
-                   (checkProofStep ([oConcept, bConcept], oRule, oConcept) []))
-otest2 = TestCase (assertEqual "Incorrect or rule proof step"
-                   ("", False, [[oConcept]])
-                   (checkProofStep ([oConcept], oRule, Or (Atom "A") (Atom "B"))
-                    gamma))
-otest3 = TestCase (assertEqual "Corret or rule proof step with gamma"
+                   $ checkProofStep ([oConcept, bConcept], oRule, oConcept) [])
+otest2 = TestCase (assertEqual "InCorrect or rule proof step"
+                   ("(Atom A or Atom B) does not exist in the set of " ++
+                    "concepts {(Atom C or Atom D)}", False, [[oConcept]])
+                   $ checkProofStep ([oConcept], oRule, Or (Atom "A") (Atom "B"))
+                    gamma)
+otest3 = TestCase (assertEqual "Correct or rule proof step with gamma"
                    ("", True, [[Atom "C", bConcept], [Atom "D", bConcept]])
-                   (checkProofStep ([oConcept, bConcept], oRule, oConcept) gamma))
+                   $ checkProofStep ([oConcept, bConcept], oRule, oConcept) gamma)
 
-orStepTests = TestList [TestLabel "ortest1" btest1,
-                        TestLabel "ortest2" btest2,
-                        TestLabel "ortest3" btest3]
+orStepTests = TestList [TestLabel "ortest1" otest1,
+                        TestLabel "ortest2" otest2,
+                        TestLabel "ortest3" otest3]
 
 etest1 = TestCase (assertEqual "Correct exists rule proof step"
                    ("", True, [[Atom "E", Atom "F"]])
-                   (checkProofStep ([eConcept, fConcept, Forall "S" (Atom "G")],
-                    eRule, eConcept) []))
-etest2 = TestCase (assertEqual "Incorrect exists rule proof step"
-                   ("", False, [[eConcept]])
-                   (checkProofStep ([eConcept], eRule, Exists "R" (Atom "A")) gamma))
+                   $ checkProofStep ([eConcept, fConcept, Forall "S" (Atom "G")],
+                    eRule, eConcept) [])
+etest2 = TestCase (assertEqual "InCorrect exists rule proof step"
+                   ("(Exists R Atom A) does not exist in the set of " ++
+                    "concepts {(Exists R Atom E)}", False, [[eConcept]])
+                   $ checkProofStep ([eConcept], eRule, Exists "R" (Atom "A")) gamma)
 etest3 = TestCase (assertEqual "Correct exists rule proof step with gamma"
                    ("", True, [[Atom "E", Atom "F"] ++ gamma])
-                   (checkProofStep ([eConcept, fConcept, Forall "S" (Atom "G")],
-                    eRule, eConcept) gamma))
+                   $ checkProofStep ([eConcept, fConcept, Forall "S" (Atom "G")],
+                    eRule, eConcept) gamma)
 
 existsStepTests = TestList [TestLabel "existstest1" etest1,
                             TestLabel "existstest2" etest2,
@@ -115,35 +124,43 @@ testcheckProofStep = do runTestTT bottomStepTests
                         runTestTT existsStepTests
 
 
--- Tests for checkProof
+-- Tests for checkTree
 leaftest1 = TestCase (assertEqual "Correct leaf" ("", True)
-                      (checkProof (NodeZero (Neg T)) []))
-leaftest2 = TestCase (assertEqual "Incorrect leaf" ("", False)
-                      (checkProof (NodeZero (Atom "A")) gamma))
+                      $ checkTree (NodeZero (Neg T)) [])
+leaftest2 = TestCase (assertEqual "InCorrect leaf"
+                      ("This proof tree does not show unsatisfiability", False)
+                      $ checkTree (NodeZero (Atom "A")) gamma)
 leaftest3 = TestCase (assertEqual "Correct leaf with gamma" ("", True)
-                      (checkProof (NodeZero (Neg T)) gamma))
+                      $ checkTree (NodeZero (Neg T)) gamma)
 
 leafTests = TestList [TestLabel "leaftest1" leaftest1,
                       TestLabel "leaftest2" leaftest2,
                       TestLabel "leaftest3" leaftest3]
 
 treetest1 = TestCase (assertEqual "Correct tree" ("", True)
-                      (checkProof tree gamma))
-treetest2 = TestCase (assertEqual "Incorrect proof step" ("", False)
-                      (checkProof (NodeOne ([Neg (Atom "B"), Atom "A"], bRule,
+                      $ checkTree tree gamma)
+treetest2 = TestCase (assertEqual "InCorrect proof step"
+                      ("Atom A and Not (Atom A) do not both exist in the " ++
+                       "set of concepts {Not (Atom B), Atom A}", False)
+                      $ checkTree (NodeOne ([Neg (Atom "B"), Atom "A"], bRule,
                                             Atom "A")
-                                           (NodeZero (Neg T))) gamma))
+                                           (NodeZero (Neg T))) gamma)
 
 treeTests = TestList [TestLabel "treetest1" treetest1,
                       TestLabel "treetest2" treetest2]
 
-testcheckProof = do runTestTT leafTests
-                    runTestTT treeTests
+testcheckTree = do runTestTT leafTests
+                   runTestTT treeTests
 
--- Tests for ProofChecker
+
+-- Tests for checkProof
 prooftest1 = TestCase (assertEqual "Correct proof" ("", True) 
-                       (proofChecker tree gamma))
+                       $ checkProof tree gamma)
+{-
+prooftest2 = TestCase (handle errorCalls (\_ -> return ()) performCall)
+  where performCall = do evaluate (proofChecker tree [Atom "N"])
+                         assertFailure "An error should be thrown"
+-}
+checkProofTests = TestList [TestLabel "prooftreetest1" prooftest1]
 
-proofCheckerTests = TestList [TestLabel "prooftreetest1" prooftest1]
-
-testProofChecker = do runTestTT proofCheckerTests
+testcheckProof = do runTestTT checkProofTests
