7284367290a90480852b55821efca4b48ae87183
Ka Wai Cheng <Ka Wai Cheng@localhost>
2010-11-24 23:49:34 +0000

    Basic tests for benchmark 2 parser

--- Parser_test.hs
+++ Parser_test.hs
@@ -17,7 +17,7 @@ b1NotTests = maplabel "Benchmark 1 parse not test" [b1nottest1, b1nottest2]
 b1AndTests = maplabel "Benchmark 1 parse and test" [b1andtest1, b1andtest2]
 b1ImpTests = maplabel "Benchmark 1 parse implies test" [b1imptest1, b1imptest2]
 b1BoxTests = maplabel "Benchmark 1 parse box test" [b1boxtest1, b1boxtest2]
-b1DiaTests = maplabel "Benchmark 1 parse box test" [b1diatest1, b1diatest2]
+b1DiaTests = maplabel "Benchmark 1 parse dia test" [b1diatest1, b1diatest2]
 
 allb1basictests = do putStrLn "==== Testing the parser for benchmark 1 concepts"
                      runTestTT b1NotTests
@@ -30,6 +30,24 @@ b1FileTests = maplabel "Benchmark 1 parse file test" [b1test1, b1test2]
 
 b1parsefiletests = do putStrLn "==== Testing the parser for benchmark 1 files"
                       runTestTT b1FileTests
+
+
+b2NotTests = maplabel "Benchmark 2 parse not test" [b2nottest1, b2nottest2]
+b2AndTests = maplabel "Benchmark 2 parse and test" [b2andtest1, b2andtest2]
+b2OrTests  = maplabel "Benchmark 2 parse or test" [b2ortest1, b2ortest2]
+b2BoxTests = maplabel "Benchmark 2 parse box test" [b2boxtest1, b2boxtest2]
+b2DiaTests = maplabel "Benchmark 2 parse dia test" [b2diatest1, b2diatest2]
+bTrueTests = maplabel "Benchmark 2 parse true test" [b2truetest1, b2truetest2]
+b2FalseTests = maplabel "Benchmark 2 parse false test" [b2falsetest1, b2falsetest2]
+
+allb2basictests = do putStrLn "==== Testing the parser for benchmark 2 concepts"
+                     runTestTT b2NotTests
+                     runTestTT b2AndTests
+                     runTestTT b2OrTests
+                     runTestTT b2BoxTests
+                     runTestTT b2DiaTests
+                     runTestTT bTrueTests
+                     runTestTT b2FalseTests
 
 -- Testing setup
 
@@ -53,56 +71,91 @@ b1dia2 = "dia(a & b)"
 b1file1 = "text to be ignored begin 1: " ++ b1not1 ++ " end"
 b1file2 = "benchmark formulas k_branch_n.txt begin "++ listallb1 ++" end"
 
+
+
+b2true1  = "true"
+b2true2  = "or(true, false)"
+b2false1 = "false"
+b2false2 = "and(true, false)"
+b2or1    = "or(a, b)"
+b2or2    = "or(a, or(b, c))"
+b2not1   = "not(atom1)"
+b2not2   = "not(not(atom1))"
+b2and1   = "and(a, b)"
+b2and2   = "and(and(a, b), c)"
+b2box1   = "box(R, a)"
+b2box2   = "box(R, and(a, b))"
+b2dia1   = "dia(R, a)"
+b2dia2   = "dia(R, and(a, b))"
+
+-- Expected results
+not1target     = [Neg (Atom "atom1")]
+not2target     = [Neg(Neg (Atom "atom1"))]
+and1target     = [And (Atom "a") (Atom "b")]
+and2target     = [And (And (Atom "a") (Atom "b")) (Atom "c")]
+implies1target = [Or (Neg (Atom "a")) (Atom "b")]
+implies2target = [Or (Neg (Atom "a")) (Or (Neg(Atom "b")) (Atom "c"))]
+box1target     = [Forall "R" (Atom "a")]
+box2target     = [Forall "R" (And (Atom "a") (Atom "b"))]
+dia1target     = [Exists "R" (Atom "a")]
+dia2target     = [Exists "R" (And (Atom "a") (Atom "b"))]
+true1target    = [T]
+true2target    = [Or T (Neg T)]
+false1target   = [Neg T]
+false2target   = [And T (Neg T)]
+or1target      = [Or (Atom "a") (Atom "b")]
+or2target      = [Or (Atom "a") (Or (Atom "b") (Atom "c"))]
+
 -- Tests for parsing benchmark 1 concepts
 b1nottest1 = testequality msg target result b1not1
   where msg    = "Failed to correctly parse a not concept from Benchmark 1"
         result = file $ lexerB1Concepts b1not1
-        target = [Neg (Atom "atom1")]
+        target = not1target
 
 b1nottest2 = testequality msg target result b1not2
   where msg    = "Failed to correctly parse a not concept from Benchmark 1"
         result = file $ lexerB1Concepts b1not2
-        target = [Neg(Neg (Atom "atom1"))]
+        target = not2target
 
 b1andtest1 = testequality msg target result b1and1
   where msg    = "Failed to correctly parse an and concept from Benchmark 1"
         result = file $ lexerB1Concepts b1and1
-        target = [And (Atom "a") (Atom "b")]
+        target = and1target
 
 b1andtest2 = testequality msg target result b1and2
   where msg    = "Failed to correctly parse an and concept from Benchmark 1"
         result = file $ lexerB1Concepts b1and2
-        target = [And (And (Atom "a") (Atom "b")) (Atom "c")]
+        target = and2target
 
 b1imptest1 = testequality msg target result b1implies1
   where msg    = "Failed to correctly parse an and concept from Benchmark 1"
         result = file $ lexerB1Concepts b1implies1
-        target = [Or (Neg (Atom "a")) (Atom "b")]
+        target = implies1target
 
 b1imptest2 = testequality msg target result b1implies2
-  where msg    = "Failed to correctly parse an and concept from Benchmark 1"
+  where msg    = "Failed to correctly parse an implies concept from Benchmark 1"
         result = file $ lexerB1Concepts b1implies2
-        target = [Or (Neg (Atom "a")) (Or (Neg(Atom "b")) (Atom "c"))]
+        target = implies2target
 
 b1boxtest1 = testequality msg target result b1box1
-  where msg    = "Failed to correctly parse a box concept from Benchmark 1"
+  where msg    = "Failed to correctly parse a implies concept from Benchmark 1"
         result = file $ lexerB1Concepts b1box1
-        target = [Forall "R" (Atom "a")]
+        target = box1target
 
 b1boxtest2 = testequality msg target result b1box2
   where msg    = "Failed to correctly parse a box concept from Benchmark 1"
         result = file $ lexerB1Concepts b1box2
-        target = [Forall "R" (And (Atom "a") (Atom "b"))]
+        target = box2target
 
 b1diatest1 = testequality msg target result b1dia1
   where msg    = "Failed to correctly parse a dia concept from Benchmark 1"
         result = file $ lexerB1Concepts b1dia1
-        target = [Exists "R" (Atom "a")]
+        target = dia1target
 
 b1diatest2 = testequality msg target result b1dia2
   where msg    = "Failed to correctly parse a dia concept from Benchmark 1"
         result = file $ lexerB1Concepts b1dia2
-        target = [Exists "R" (And (Atom "a") (Atom "b"))]
+        target = dia2target
 
 -- Tests for parsing benchmark 1 files
 b1test1 = testequality msg target result b1file1
@@ -128,4 +181,73 @@ btest3 = testequality msg target result "k_branch_n.txt"
         target = [Neg (Atom "atom1")]
 -}
 
+-- Tests for parsing benchmark 2 concepts
+b2nottest1 = testequality msg target result b2not1
+  where msg    = "Failed to correctly parse a not concept from Benchmark 2"
+        result = file $ lexerB2Concept b2not1
+        target = not1target
 
+b2nottest2 = testequality msg target result b2not2
+  where msg    = "Failed to correctly parse a not concept from Benchmark 2"
+        result = file $ lexerB2Concept b2not2
+        target = not2target
+
+b2andtest1 = testequality msg target result b2and1
+  where msg    = "Failed to correctly parse an and concept from Benchmark 2"
+        result = file $ lexerB2Concept b2and1
+        target = and1target
+
+b2andtest2 = testequality msg target result b2and2
+  where msg    = "Failed to correctly parse an and concept from Benchmark 2"
+        result = file $ lexerB2Concept b2and2
+        target = and2target
+
+b2ortest1 = testequality msg target result b2or1
+  where msg    = "Failed to correctly parse an or concept from Benchmark 2"
+        result = file $ lexerB2Concept b2or1
+        target = or1target
+
+b2ortest2 = testequality msg target result b2or2
+  where msg    = "Failed to correctly parse an or concept from Benchmark 2"
+        result = file $ lexerB2Concept b2or2
+        target = or2target
+
+b2boxtest1 = testequality msg target result b2box1
+  where msg    = "Failed to correctly parse a box concept from Benchmark 2"
+        result = file $ lexerB2Concept b2box1
+        target = box1target
+
+b2boxtest2 = testequality msg target result b2box2
+  where msg    = "Failed to correctly parse a box concept from Benchmark 2"
+        result = file $ lexerB2Concept b2box2
+        target = box2target
+
+b2diatest1 = testequality msg target result b2dia1
+  where msg    = "Failed to correctly parse a dia concept from Benchmark 2"
+        result = file $ lexerB2Concept b2dia1
+        target = dia1target
+
+b2diatest2 = testequality msg target result b2dia2
+  where msg    = "Failed to correctly parse a dia concept from Benchmark 2"
+        result = file $ lexerB2Concept b2dia2
+        target = dia2target
+
+b2truetest1 = testequality msg target result b2true1
+  where msg    = "Failed to correctly parse a true concept from Benchmark 2"
+        result = file $ lexerB2Concept b2true1
+        target = true1target
+
+b2truetest2 = testequality msg target result b2true2
+  where msg    = "Failed to correctly parse a true concept from Benchmark 2"
+        result = file $ lexerB2Concept b2true2
+        target = true2target
+
+b2falsetest1 = testequality msg target result b2false1
+  where msg    = "Failed to correctly parse a false concept from Benchmark 2"
+        result = file $ lexerB2Concept b2false1
+        target = false1target
+
+b2falsetest2 = testequality msg target result b2false2
+  where msg    = "Failed to correctly parse a false concept from Benchmark 2"
+        result = file $ lexerB2Concept b2false2
+        target = false2target
