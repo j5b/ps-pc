62660372ab1d68b373958c068232c185cf14d72e
Michal Parusinski <mparusinski@googlemail.com>
2010-11-29 15:54:10 +0000

    Cycle testing

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
+if [ -e Parser.hs ]
+then
+    echo "Parser.hs found, which is good"
+else
+    echo "Compiling Parser.hs"
+    happy Parser.y
+fi
+runhaskell Setup.hs configure
 runhaskell Setup.hs test
