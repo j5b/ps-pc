7a2ecf32d0bdc7db62cf6a016c3514a2e3c8ae48
Ka Wai Cheng <Ka Wai Cheng@localhost>
2010-12-12 00:48:19 +0000

    Adds tests for OutputModel and tidies up some code in OutputModel

--- OutputModel.hs
+++ OutputModel.hs
@@ -14,39 +14,34 @@ import Signature
 import Model
 import ProofSearch
 
-
--- Strategy: draw unaries then draw elements in domain that do not appear in unaries, then draw the relations
+{-
+ Output string of DOT language that draws model
+ WARNING: assumes the model is already checked & correct
+-}
 modelToGraph :: Model -> String
-modelToGraph ([], _, _) = ""
+modelToGraph ([], _, _) = "Domain is empty, no model to draw"
 modelToGraph (dom, us, bs)
-  = "digraph { \n" ++ unaryToGraph (mapUnary us empty) ++
-    domOnlyToGraph dom us ++ relToGraph bs ++ " }"
-
--- TO DO: Draw relations
-relToGraph :: [BinaryRelation] -> String
-relToGraph [] = []
-relToGraph ((r, edges):rs) = drawEdges r edges ++ relToGraph rs
+  = "digraph {\n " ++ concat ulabels ++
+    domOnlyToGraph (nub dom Data.List.\\ unodes) ++
+    concatMap drawEdges (nub bs) ++ "}"
+  where (unodes, ulabels) = unaryToGraph (mapUnary (nub us) empty)
 
 -- Draws edges for a binary relation
-drawEdges :: String -> [(Individual, Individual)] -> String
-drawEdges r []         = []
-drawEdges r ((p,c):es) = concat [show p, " -> ", show c, " [label = \"", r, "\"] ;\n ", drawEdges r es]
+drawEdges :: (String, [(Individual, Individual)]) -> String
+drawEdges (_, [])       = []
+drawEdges (r, (p,c):es)
+  = concat [show p, " -> ", show c, " [label=\"", r, "\"] ;\n ",
+    drawEdges (r, nub es Data.List.\\ [(p,c)])]
 
 -- Returns string that draws all individuals that are not in unary relations
-domOnlyToGraph :: [Individual] -> [UnaryRelation] -> String
-domOnlyToGraph ds us = concat [show x ++ ";\n" | x <- xs]
-  where xs = domOnly ds us
-
--- Returns individuals that do not have unary relations
-domOnly :: [Individual] -> [UnaryRelation] -> [Individual]
-domOnly [] _           = []
-domOnly d []           = d
-domOnly d ((_,is):xs)  = domOnly (d Data.List.\\ is) xs
+domOnlyToGraph :: [Individual] -> String
+domOnlyToGraph ds = concat [show i ++ " [label=\"" ++ show i ++ "\"] ;\n " | i <- ds]
 
 -- Returns string to label every unary in the 
-unaryToGraph :: Map Individual String -> String
-unaryToGraph m = "\\* Nodes for individuals *\\ \n " ++ concat [show k ++ " [label=\"" ++ show k ++ ": " ++ a ++ "\"] ;\n" | (k, a) <- list ]
- where list = Data.Map.toList m
+unaryToGraph :: Map Individual String -> ([Individual], [String])
+unaryToGraph m
+  = unzip [(k, show k ++ " [label=\"" ++ show k ++ ": " ++ a ++ "\"] ;\n ")
+           | (k, a) <- Data.Map.toList m ]
 
 -- Collects all dot labels
 mapUnary :: [UnaryRelation] -> Map Individual String -> Map Individual String
@@ -58,4 +53,3 @@ addToIndividual :: String -> [Individual] -> Map Individual String
                    -> Map Individual String
 addToIndividual u [] m     = m
 addToIndividual u (i:is) m = insertWith (++) i (u ++ " ") $ addToIndividual u is m
- 
