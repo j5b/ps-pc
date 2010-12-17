21209cc201e9deda07a289fc9081758000dbc790
Jannis Bulian <jannis.bulian@gmail.com>
2010-12-17 17:21:19 +0000

    Sorts the model now.

---
+++ Setup.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+import Distribution.Simple
+import Distribution.Simple.UserHooks
+import Distribution.PackageDescription
+import TestMain
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks {
+         runTests = runTestsPSPC }
+
