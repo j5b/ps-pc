ab93a6c45edde94b15f3338e8d74a57fca1cdbd8
Ka Wai Cheng <Ka Wai Cheng@localhost>
2010-12-23 11:39:40 +0000

    Nicer display of node content

--- OutputModel_test.hs
+++ OutputModel_test.hs
@@ -14,13 +14,17 @@ import TestUtils
 import Test.HUnit
 
 domainTests = maplabel "Output model domain test"
-  [domaintest1, domaintest2, domaintest3, domaintest4, nodesizetest]
+  [domaintest1, domaintest2, domaintest3, domaintest4]
+
+nodesizeTests = maplabel "Output model nodesize test"
+  [nodesizetest1, nodesizetest2, nodesizetest3, nodesizetest4]
 
 errorTests = maplabel "Output model error test"
   [errortest1, errortest2, errortest3, errortest4]
 
 alltests = do putStrLn "==== Testing the model output"
               runTestTT domainTests
+              runTestTT nodesizeTests
               runTestTT errorTests
 
 
@@ -31,7 +35,7 @@ dom1 = [1]
 dom2 = [1,2,3]
 dom3 = [1,2,3,4]
 dom4 = [1,2,3,4,5,6]
-domall = concat [dom1, dom2]
+domall = dom1 ++ dom2
 
 unary0 = []
 unary1 = [("A",[])]
@@ -45,7 +49,7 @@ binary1 = [("R",[(1,1)])]
 binary2 = [("R",[(1,2),(1,2)])]
 binary3 = [("R",[(1,2),(2,3)])]
 binary4 = [("A",[(4,1)]),("R",[(4,1)])]
-binaryall = concat [binary1, binary4]
+binaryall = binary1 ++ binary4
 
 -- Expected results
 
@@ -67,33 +71,52 @@ domaintest2 = testequality msg target result
               "([1,2,3],[(B,[1])], [(R,[(1,2),(2,1)])])"
   where msg    = "Failed to produce correct output for simple model"
         result = modelToGraph (dom2, unary2, binary2)
-        target = begin ++ "1 [label=\"1: B \"] ;\n 2 [label=\"2\"] ;\n 3 " ++
+        target = begin ++ "1 [label=\"1: B\"] ;\n 2 [label=\"2\"] ;\n 3 " ++
                  "[label=\"3\"] ;\n 1 -> 2 [label=\"R\"] ;\n " ++ end
 
 domaintest3 = testequality msg target result
               "([1,2,3,4],[(C,[1]), (D,[2,3])], [(R,[(1,2),(2,3)])])"
   where msg    = "Failed to produce correct output for simple model"
         result = modelToGraph (dom3, unary3, binary3)
-        target = begin ++ "1 [label=\"1: C \"] ;\n 2 [label=\"2: D \"] ;\n " ++
-                 "3 [label=\"3: D \"] ;\n 4 [label=\"4\"] ;\n 1 -> 2 " ++
+        target = begin ++ "1 [label=\"1: C\"] ;\n 2 [label=\"2: D\"] ;\n " ++
+                 "3 [label=\"3: D\"] ;\n 4 [label=\"4\"] ;\n 1 -> 2 " ++
                  "[label=\"R\"] ;\n 2 -> 3 [label=\"R\"] ;\n " ++ end
 
 domaintest4 = testequality msg target result "([1,1,2,3,1,2,3,4,5,6],[(E,[1,3]), (A,[2,4,1])], [(A,[(4,1)]),(R,[(4,1)])])"
   where msg    = "Failed to produce correct output for simple model"
         result = modelToGraph (dom4, unary4, binary4)
-        target = begin ++ "1 [label=\"1: A E \"] ;\n 2 [label=\"2: A \"] ;\n" ++
-                 " 3 [label=\"3: E \"] ;\n 4 [label=\"4: A \"] ;\n 5 " ++
+        target = begin ++ "1 [label=\"1: A, E\"] ;\n 2 [label=\"2: A\"] ;\n" ++
+                 " 3 [label=\"3: E\"] ;\n 4 [label=\"4: A\"] ;\n 5 " ++
                  "[label=\"5\"] ;\n 6 [label=\"6\"] ;\n 4 -> 1 " ++
                  "[label=\"A\"] ;\n 4 -> 1 [label=\"R\"] ;\n " ++ end
 
-nodesizetest = testequality msg target result
+nodesizetest1 = testequality msg target result
   "([1,2,3,4,5,6],[(B,[1]),(C,[1]),(D,[2,3]),(E,[1,3]),(A,[2,4,1])], [(A,[(4,1)]),(R,[(4,1)])]))"
-  where msg    = "Failed to produce correct output for simple model"
+  where msg    = "Failed to produce correct output for many unary relations"
         result = modelToGraph (dom4, unaryall, binary4)
-        target = begin ++ "1 [label=\"1: A E C \\nB \"] ;\n 2 [label=\"2: " ++
-                 "A D \"] ;\n 3 [label=\"3: E D \"] ;\n 4 [label=\"4: A \"]" ++
+        target = begin ++ "1 [label=\"1: A, E, C, \\nB\"] ;\n 2 [label=\"2: " ++
+                 "A, D\"] ;\n 3 [label=\"3: E, D\"] ;\n 4 [label=\"4: A\"]" ++
                  " ;\n 5 [label=\"5\"] ;\n 6 [label=\"6\"] ;\n 4 -> 1 " ++
                  "[label=\"A\"] ;\n 4 -> 1 [label=\"R\"] ;\n " ++ end
+
+-- Unary name of just right length (8 characters)
+nodesizetest2 = testequality msg target result "([1],[(justsize,[1])],[])"
+  where msg    = "Failed to produce correct output for short length unary name"
+        result = modelToGraph ([1], [("justsize",[1])], [])
+        target = begin ++ "1 [label=\"1: justsize\"] ;\n " ++ end
+
+-- Unary name of just over length (9 characters)
+nodesizetest3 = testequality msg target result "([1],[(istoolong,[1])],[])"
+  where msg    = "Failed to produce correct output for simple model"
+        result = modelToGraph ([1], [("istoolong",[1])], [])
+        target = begin ++ "1 [label=\"1: istoolo-\\nng\"] ;\n " ++ end
+
+nodesizetest4 = testequality msg target result "([1],[(A,[1]),(justsize,[1]),(B,[1]),(C,[1]),(istoolong,[1]),(D,[1]),(E,[1])],[])"
+  where msg    = "Failed to produce correct output for mixture of unary name lengths"
+        result = modelToGraph ([1], [("A",[1]),("justsize",[1]),("B",[1]),
+                 ("C",[1]),("istoolong",[1]),("D",[1]),("E",[1])], [])
+        target = begin ++ "1 [label=\"1: E, D, \\nistoolo-\\nng, C, B, \\n" ++
+                 "justsize, \\nA\"] ;\n " ++ end
 
 errortest1 = testequality msg target result "empty model ([],[],[])"
   where msg    = "Failed to detect empty domain"
