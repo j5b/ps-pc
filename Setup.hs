2cb02afc34c2c95ec7743a76c87266bf866e1a3a
Saguy Benaim <sbenaim@hotmail.co.uk>
2011-01-09 21:25:38 +0000

    sagie part

---
+++ Setup.hs
@@ -0,0 +1,10 @@
+module Main (main) where
+
+import Distribution.Simple
+import Distribution.Simple.UserHooks
+import Distribution.PackageDescription
+--import TestMain
+
+main :: IO ()
+main = defaultMain
+
