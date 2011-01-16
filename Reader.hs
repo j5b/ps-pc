086bfe33c58ffba6d81abb3dc811d128bfea4b41
Michal Parusinski <mparusinski@googlemail.com>
2011-01-16 16:40:39 +0000

    Proper packaging

--- Reader.hs
+++ Reader.hs
@@ -22,26 +22,37 @@ import OutputModel
 import OutputProof
 import ModelChecker
 import ProofChecker
+import HelpText
 
 type Gamma = [Concept]
 type Givens = [Concept]
 type Data = (Gamma, Givens)
 
+-- Data type for output form excepted
 data OutputMode = None | Console | Graphical | Check
                   deriving (Show, Eq)
                  
+-- Data type to handle Gamma in files and directly given in command line
 data GammaInput  = DirectGamma String | FileGamma FilePath
                    deriving (Show, Eq)
+
+-- Data type to handle Givens in files and directly given in command line
 data GivensInput = DirectGivens String | FileGivens FilePath
                    deriving (Show, Eq)
+
+-- Data type to specify the name for the output
 data OutputFile  = DefaultLocation | Location FilePath
                    deriving (Show, Eq)
 
+-- Associates all the data type above to provide the information
+-- about the request from the user
 data Info = Gamma GammaInput | Givens GivensInput
           | OutFile OutputFile | OutMode OutputMode
           | HelpRequest
             deriving (Show, Eq)
 
+-- Data type to express a command. Seperates it from retrieving 
+-- the information
 data Command = Solve OutputMode Data FilePath | Help
   deriving (Show, Eq)
 
@@ -49,18 +60,23 @@ readLocation :: OutputFile -> String
 readLocation DefaultLocation = "output"
 readLocation (Location file) = file
 
+-- Read gamma from command line or file
+-- note: it removes newlines, tabs to allow more freedom in format for files
 readGamma :: GammaInput -> IO String
 readGamma (DirectGamma gamma) = return gamma
 readGamma (FileGamma file) 
     = do content <- readFile file
          return $ filter (\x -> not $ elem x "\n\t\b") content
 
+-- Read givens from command line or file
+-- note: it removes newlines, tabs to allow more freedom in format for files
 readGivens :: GivensInput -> IO String
 readGivens (DirectGivens givens) = return givens
 readGivens (FileGivens file) 
     = do content <- readFile file
          return $ filter (\x -> not $ elem x "\n\t\b") content
 
+-- Reads the output mode from a string
 getMode :: String -> OutputMode
 getMode ('n':'o':'n':'e':rest) = None
 getMode ('c':'o':'n':'s':'o':'l':'e':rest) = Console
@@ -69,8 +85,8 @@ getMode ('c':'h':'e':'c':'k':rest) = Check
 getMode _ 
     = error "Unable to process mode: please write 'none', 'console' or 'graphical'"
 
-
--- is str1 in str2 or vice versa partially
+-- Function to answer:
+-- is str1 in str2 or vice versa partially?
 match :: String -> String -> Bool
 match str1 str2 = match' str1 str2
     where match' [] _ = True
@@ -83,6 +99,7 @@ match str1 str2 = match' str1 str2
 isFilePath :: String -> Bool
 isFilePath string = length (filter (== '.') string) == 1
 
+-- Takes the information obtained and turns into a command
 createCommand :: [Info] -> IO Command
 createCommand [] = error "Can't do nothing with no arguments please refer to help"
 createCommand infos
@@ -96,6 +113,8 @@ createCommand infos
     where (gm,gi,outf,outmode) = processInfo infos
           outfile = readLocation outf
 
+-- Help function for the one above: it extract the information from the list
+-- and adds default parameters if some options haven't been specified
 processInfo :: [Info] -> (GammaInput, GivensInput, OutputFile, OutputMode)
 processInfo [] = error "Unexpected error in processInfo, please contact developer"
 processInfo [Gamma gamma] 
@@ -151,10 +170,7 @@ processArgs (option:matching:rest)
          return $ OutMode (getMode matching):restinfo
     | otherwise = error $ "Unable to understand argument "++option++" please refer to help"
 
-helpString :: IO String
-helpString = readFile "HELP.TXT"
-
--- output the result of the proof/model searcher
+-- Outputs the result of the proof/model searcher
 outputResult :: OutputMode -> Data -> Either Model ProofTree -> FilePath -> IO ()
 outputResult None _ result _             = either left right result
   where left  x = putStrLn "SATISFIABLE"
@@ -162,9 +178,9 @@ outputResult None _ result _             = either left right result
 outputResult Console _ result filename   = resultToConsole result filename
 outputResult Graphical _ result filename = either left right result
   where left  x = do outputModel x filename "png" 
-                     putStrLn $ "A generated model has been outputed to models/"++filename++".png"
-        right x = do outputProof x $ "proofs/"++filename
-                     putStrLn $ "A generated proof has been outputed to proofs/"++filename++".pdf"
+                     putStrLn $ "A generated model has been outputed to "++filename++".png"
+        right x = do outputProof x filename
+                     putStrLn $ "A generated proof has been outputed to "++filename++".pdf"
 outputResult Check (gm,gi) result filename 
     = do resultToConsole result filename 
          let answer = either left right result 
@@ -175,7 +191,7 @@ outputResult Check (gm,gi) result filename
   where left x  = checkInputModel x gm gi
         right x = checkProof x (map toNNF gm)
 
--- executes command given
+-- Executes command given
 executeCommand :: Command -> IO ()
 executeCommand (Solve mode (gamma, givens) filename)
   = do putStrLn $ "For GAMMA: "++conceptsToConsole gamma
@@ -185,5 +201,4 @@ executeCommand (Solve mode (gamma, givens) filename)
           else putStrLn "The result will be generated in a seperate file (indicated by --output if specified)"
        outputResult mode (gamma, givens) (findPOM givens gamma) filename
 executeCommand Help
-  = do output <- helpString
-       putStrLn output
+  = putStrLn help
