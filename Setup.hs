5cd6f7e38d84a5daac9c3819ef6c4fce8502b10a
Michal Parusinski <mparusinski@googlemail.com>
2011-01-16 16:49:01 +0000

    small change

--- Setup.hs
+++ Setup.hs
@@ -9,25 +9,6 @@ import System.Cmd(system)
 import System.Exit
 --import TestMain
 
-postconfiguration :: Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-postconfiguration _ _ _ _ -- ignores everything
-    = do checklatex
-         checkgraphivz
-
-checklatex :: IO ()
-checklatex = do putStrLn "checking for latex"
-                latexcode <- system "pdflatex --version"
-                if latexcode /= ExitSuccess 
-                   then error "Make sure latex is installed"
-                   else return ()
-
-checkgraphivz :: IO ()
-checkgraphivz = do putStrLn "checking for graphviz"
-                   graphivzcode <- system "dot --version"
-                   if graphivzcode /= ExitSuccess
-                      then error "Make sure graphivz is installed with the program dot included"
-                      else return ()
-
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
      { postConf = postconfiguration }
